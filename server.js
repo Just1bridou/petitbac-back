52b34bd3944cfbecd5a6a6f07f064886412b8d39
Justin A-M <justin@omnibook.com>
2023-02-21 22:44:44 +0100

    fix: cors

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: [
+      "http://localhost:3000",
+      "http://91.121.75.14:23:3003",
+      "http://91.121.75.14:23:3000",
+    ],
     methods: ["GET", "POST"],
   },
 });
